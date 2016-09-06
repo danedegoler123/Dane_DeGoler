ebd0b78f4fe3c7adf52b8bd0d1ae081e68967c55
danedegoler123 <dane.degoler@yahoo.com>
2016-09-06 10:19:23 -0700

    Lab 2

---
+++ PyLesson_01/Lab_02.py
@@ -0,0 +1,15 @@
+5*6
+print ("5 multiplied by 6 is 30")
+
+
+name = "Avenida Maravillas"
+adress = 5266
+city= "San Diego, CA"
+zip= 92067
+print (adress, name, city, zip)
+
+H = 5
+L = 6
+W = 7
+recSurf= W*L+W*L+H*L+H*L+H*W+H*W
+print ("the surface area of your recangle is", recSurf)
