b637af22edcfccbfb369ef0ef0f6e9c4ff0341a9
danedegoler123 <dane.degoler@yahoo.com>
2016-10-12 09:53:35 -0700

    Finished lesson_05.1

---
+++ PyLesson_05/Lesson_05.1BMI.py
@@ -0,0 +1,29 @@
+height = int(input("Enter your height in inches: "))
+weight = int(input("Enter your weight in pounds: "))
+
+condition = (weight / (height * height)) * 703
+
+def calcBMI(condition):
+    if condition < 18.5:
+        print("You are underweight")
+
+    elif condition <= 24.9:
+        print("You are normal")
+
+    elif condition <= 29.9:
+        print("You are overweight")
+
+    elif condition <= 34.9:
+        print("You are obese")
+
+    elif condition <= 39.9:
+        print("You are very obese")
+
+    if condition > 39.9:
+        print("You are morbidly obese")
+
+        
+calcBMI(condition)
+
+print("Your BMI is", condition)
+
---
+++ PyLesson_05/Lesson_05.1GPA.py
@@ -0,0 +1,32 @@
+grade1 = input("Enter the letter grade for your first class: ")
+grade2 = input("Enter the letter grade for your second class: ")
+grade3 = input("Enter the letter grade for your third class: ")
+grade4 = input("Enter the letter grade for your fourth class: ")
+grade5 = input("Enter the letter grade for your fifth class: ")
+grade6 = input("Enter the letter grade for your sixth class: ")
+grade7 = input("Enter the letter grade for your seventh class: ")
+
+def calcPoints(grade):
+    if grade == "A":
+        return 4.0
+    if grade == "B":
+        return 3.0
+    if grade == "C":
+        return 2.0
+    if grade == "D":
+        return 1.0
+    if grade == "F":
+        return 0.0
+
+calcPoints(grade1)
+calcPoints(grade2)
+calcPoints(grade3)
+calcPoints(grade4)
+calcPoints(grade5)
+calcPoints(grade6)
+calcPoints(grade7)
+
+gpa = (calcPoints(grade1) + calcPoints(grade2) +calcPoints(grade3) +calcPoints(grade4) + calcPoints(grade5) + calcPoints(grade6) + calcPoints(grade7))/7
+                                                                                                                                       
+print("Your GPA is", gpa)
+
