1c6bb76a463ab588f3a61efb243a9aaa5ab0d60d
danedegoler123 <dane.degoler@yahoo.com>
2016-11-01 10:25:01 -0700

    Finished lesson_07Ex_02

--- PyLesson_07/Lesson_07Ex_02.py
+++ PyLesson_07/Lesson_07Ex_02.py
@@ -1,5 +1,17 @@
 number = int(input("Please enter a number: "))
 digits = 0
 average = 0
+num = number
 
-def avDigits
+def avDigits (num, average, digits):
+    while num > 0:
+        digits += 1
+        average += num % 10
+        num = int(num / 10)
+    average /= digits
+    return average
+print("The average digits in", number ,"is", avDigits(num, average, digits)) 
+
+
+
+
