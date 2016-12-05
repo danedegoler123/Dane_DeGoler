b03b322d1fc4636f3ad9ac8c991c105818d88b12
danedegoler123 <dane.degoler@yahoo.com>
2016-12-05 10:12:40 -0800

    Added Lesson_09 Ex_04 and Ex_05

---
+++ PyLesson_09/Lesson_09Ex_04.py
@@ -0,0 +1,20 @@
+start = int(input("Enter your starting number: "))
+size = int(input("Enter your sequence size: "))
+
+seq = [] 
+out = ""
+for i in range(0,size):
+    
+    if i == 0 or i == 1:
+        seq.append(start)
+    else:
+        seq.append(seq[i-1] + seq[i-2])
+    out += str(seq[i]) + " "
+    
+print(out)
+
+
+
+
+
+
