91a408391858171839ec83dbd8c30d22b0a0d60d
danedegoler123 <dane.degoler@yahoo.com>
2016-12-06 09:56:45 -0800

    Finished Lesson_09Ex_06

---
+++ PyLesson_09/Lesson_09Ex_06.py
@@ -0,0 +1,30 @@
+mathematicalExpression = input("Please enter a mathematical expression with spaces: ")
+equation = mathematicalExpression.split()
+i = 0
+while i < len(equation):
+    if i < len(equation) and equation[i] == "*" or "/":
+        if equation[i] == "*":
+            equation[i] = int(equation[i-1]) * int(equation[i+1])
+            equation.pop(i-1)
+
+            equation.pop(i)
+        elif equation[i] == "/":
+            equation[i] = int(equation[i-1]) / int(equation[i+1])
+            equation.pop(i-1)
+            equation.pop(i)
+    i += 1
+i = 0
+while i < len(equation):
+    if i < len(equation) and equation[i] == "+" or "-":
+        if equation[i] == "+":
+            equation[i] = int(equation[i-1]) + int(equation[i+1])
+            equation.pop(i-1)
+            equation.pop(i)
+        elif equation[i] == "-":
+            equation[i] = int(equation[i-1]) - int(equation[i+1])
+            equation.pop(i-1)
+            equation.pop(i)
+    i += 1
+print(equation)
+
+
